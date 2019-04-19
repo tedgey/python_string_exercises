9b6d1ac3d147b8bba12ffaffcddd287a54b98ac3
Thomas Edgeworth <thomasedgeworth@Thomass-MacBook-Air.local>
2019-04-18 20:18:40 -0400

    first commit

---
+++ all_string_exercises.py
@@ -0,0 +1,81 @@
+    # Uppercase a String
+
+# example = "this is a string"
+# example2 = example.upper()
+
+# print(example2)
+
+#     Capitalize a String
+
+# example = "this is a string"
+# example2 = example.capitalize()
+
+# print(example2)
+
+#     Reverse a String
+
+# example = "this is a string"
+# example2 = example[::-1]
+
+# print(example2)
+
+#     Leetspeak
+
+# paragraph = "Do you like green eggs and ham? I do not like them, Sam-I-Am. I do not like green eggs and ham."
+# leet_1 = ""
+# leet_2 = ""
+# leet_3 = ""
+# leet_4 = ""
+# leet_5 = ""
+# leet_6 = ""
+# leet_7 = ""
+
+# print(paragraph)
+
+
+# for letter in paragraph:
+#     if letter in paragraph:
+#             leet_1 = paragraph.replace("a", "4")
+
+# for letter in leet_1:
+#     if letter in leet_1:
+#         leet_2 = leet_1.replace("e", "3")
+            
+# for letter in leet_2:
+#     if letter in leet_2:
+#         leet_3 = leet_2.replace("g", "6")
+
+# for letter in leet_3:
+#     if letter in leet_3:
+#         leet_4 = leet_3.replace("i", "1")            
+
+# for letter in leet_4:
+#     if letter in leet_4:
+#         leet_5 = leet_4.replace("o", "0")       
+
+# for letter in leet_5:
+#     if letter in leet_5:
+#         leet_6 = leet_5.replace("s", "5")       
+
+# for letter in leet_6:
+#     if letter in leet_6:
+#         leet_7 = leet_6.replace("t", "7")       
+
+# print(leet_7)
+
+#    Long-long vowels
+
+# word = ""
+# longword = ""
+
+# def longify(word, longword):
+#     for oos in word:
+#         longword = word.replace("oo", "ooooo")
+#     for ees in word:
+#         longword = word.replace("ee", "eeeee")
+#     return print(longword)
+
+# longify("ree", longword)
+
+    # Caesar Cipher
+
